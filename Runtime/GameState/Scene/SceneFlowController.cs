441ab3ad7e492c44e0e2a24c8fa979acbd66023a
Jimothy <jimothypepperoni@gmail.com>
2025-05-15 02:31:00 +0000

    Give the abstract scene flow controller a reference to game state manager

--- Runtime/GameState/Scene/SceneFlowController.cs
+++ Runtime/GameState/Scene/SceneFlowController.cs
@@ -7,10 +7,12 @@ namespace Jimothy.Systems.GameState.Scene
     public abstract class SceneFlowController : MonoBehaviour
     {
         protected IGameFlowController GameFlowController;
+        protected IGameStateManager GameStateManager;
 
         protected virtual void Awake()
         {
             ServiceLocator.Global.Get(out GameFlowController);
+            ServiceLocator.Global.Get(out GameStateManager);
         }
     }
 }
